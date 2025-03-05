1198b7bd3274137a2481be3d096fbf65c1c997a0
Alexis Gadbin <alexis@gadbin.com>
2025-03-05 19:07:38 +0100

    Initial Commit

---
+++ Bookster/BooksterApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksterApp.swift
+//  Bookster
+//
+//  Created by Alexis Gadbin on 05/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct BooksterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
